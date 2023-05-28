6afd2935c9f59c77862225d97af04e782b57de24
Joseph Perez <joperez@hotmail.fr>
2023-05-28 15:32:28 +0200

    Only enable loom with test

--- src/loom.rs
+++ src/loom.rs
@@ -1,14 +1,15 @@
 #![allow(unused_imports)]
+use std::sync::atomic;
 /// Do not export UnsafeCell because it's used for a shared slice, so not compatible with loom model
 
-#[cfg(not(loom))]
-pub(crate) use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-#[cfg(not(loom))]
+#[cfg(not(all(loom, test)))]
+pub(crate) use std::sync::atomic::{AtomicUsize, Ordering};
+#[cfg(not(all(loom, test)))]
 #[cfg(feature = "sync")]
-pub(crate) use std::sync::{Condvar, Mutex, MutexGuard};
+pub(crate) use std::sync::{atomic::AtomicBool, Condvar, Mutex, MutexGuard};
 
-#[cfg(loom)]
-pub(crate) use loom::sync::{
-    atomic::{AtomicBool, AtomicUsize, Ordering},
-    Condvar, Mutex, MutexGuard,
-};
+#[cfg(all(loom, test))]
+pub(crate) use loom::sync::atomic::{AtomicUsize, Ordering};
+#[cfg(all(loom, test))]
+#[cfg(feature = "sync")]
+pub(crate) use loom::sync::{atomic::AtomicBool, Condvar, Mutex, MutexGuard};
